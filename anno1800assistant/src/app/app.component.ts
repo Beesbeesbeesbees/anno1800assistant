6fa493ce36bfbc058c2144cf2ecd0cb46d0db2e2
Kyle <kbugdal@gmail.com>
2019-04-16 20:51:22 -0400

    Checking in some work

--- anno1800assistant/src/app/app.component.ts
+++ anno1800assistant/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'anno1800assistant';
+export class AppComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+    this.reset();
+  }
+
+  reset() {    
+    this.islands = [new Island("First Island")];
+  }
+
+  islands: Island[]
+}
+
+
+export class Island {
+  Name: string
+
+  constructor(name: string) {
+    this.Name = name;
+  }
 }
