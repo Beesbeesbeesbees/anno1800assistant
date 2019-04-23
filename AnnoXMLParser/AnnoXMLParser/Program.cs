acaa3db5bbef38205a4590e756aa536bd673c754
Kyle <kbugdal@gmail.com>
2019-04-23 11:58:19 -0400

    Add support for FreeAreaBuildings from XML (Lumberjack huts, Hunting cabins, etc.)

--- AnnoXMLParser/AnnoXMLParser/Program.cs
+++ AnnoXMLParser/AnnoXMLParser/Program.cs
@@ -24,7 +24,14 @@ namespace AnnoXMLParser {
             while (queue.Any()) {
                 var element = queue.Pop();
 
-                if (element.Name == "Template" && (element.Value == "FactoryBuilding7" || element.Value == "FarmBuilding" || element.Value == "HeavyFactoryBuilding" || element.Value == "PublicServiceBuilding" || element.Value == "SlotFactoryBuilding7")) {
+                if (element.Name == "Template" && (
+                    element.Value == "FactoryBuilding7"
+                        || element.Value == "FarmBuilding"
+                        || element.Value == "HeavyFactoryBuilding"
+                        || element.Value == "PublicServiceBuilding"
+                        || element.Value == "SlotFactoryBuilding7"
+                        || element.Value == "FreeAreaBuilding"
+                )) {
                     factoryXMLs.Add(element.Parent);
                 }
 
